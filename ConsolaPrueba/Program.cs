50b1c5cad1ef149aac08061e440b5bc4facf913d
Eliza2692 <eliza2692@hotmail.com>
2016-09-17 15:33:40 -0400

    hola

--- ConsolaPrueba/Program.cs
+++ ConsolaPrueba/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ConsolaPrueba
 {
@@ -10,7 +11,8 @@ namespace ConsolaPrueba
     {
         static void Main(string[] args)
         {
-            
+            StreamWriter sw = new StreamWriter();
+
         }
     }
 }
